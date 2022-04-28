9d9327340e37aed84b6907ff8f3673b90aa19479
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-04-27 21:55:50 -0400

    Time: 107 ms (65.59%), Space: 50.9 MB (60.78%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,20 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    
-     let numberCount = {};
-    for (i of nums) {
-        if (numberCount[i]) {
-            numberCount[i] += 1
-        } else {
-            numberCount[i] = 1
-        }
-    }
-
-    for (i in numberCount){
-        if (numberCount[i] > 1){
-            return true
-        }
-    }
-    return false
+    const setNums = new Set(nums)
+     return setNums.size !== nums.length
 };
