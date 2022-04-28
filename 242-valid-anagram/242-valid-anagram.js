42993808affc683d3c54c3d6ee67197ac3cb2a55
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-04-27 22:27:33 -0400

    Time: 140 ms (21.08%), Space: 44.7 MB (40.41%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -7,14 +7,14 @@ var isAnagram = function(s, t) {
    const letterCountS = {}
     const letterCountT = {}
 
-    for (char of s) {
+    for (let char of s) {
         if (letterCountS[char]) {
             letterCountS[char] +=1 
         } else {
             letterCountS[char] = 1
         }
     }
-    for (char of t) {
+    for (let char of t) {
         if (letterCountT[char]) {
             letterCountT[char] +=1 
         } else {
@@ -22,7 +22,7 @@ var isAnagram = function(s, t) {
         }
     }
 
-    for (i in letterCountS) {
+    for (let i in letterCountS) {
         if (letterCountT[i] !== letterCountS[i] || Object.values(letterCountS).length !== Object.values(letterCountT).length) {
             return false
         }
