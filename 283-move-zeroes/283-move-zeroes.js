b7d52e7005967d47a755e0800f0b4cb6af3bcb6b
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-04-29 22:01:29 -0400

    Time: 237 ms (7.78%), Space: 47.3 MB (10.74%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -3,11 +3,12 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    for (let num of nums) {
-        if (num === 0) {
-            nums.splice(nums.indexOf(num), 1)
-            nums.push(num)
+    
+   for (let num of nums) {
+            if (num === 0) {
+                nums.splice(nums.indexOf(num), 1)
+                nums.push(num)
+            }
         }
-    }
-    return nums
+        return nums
 };
