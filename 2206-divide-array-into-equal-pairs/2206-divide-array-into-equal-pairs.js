bfa95e875d01ef5a614263bd87780486579cd594
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-04-30 15:08:12 -0400

    Time: 573 ms (5.29%), Space: 48.2 MB (6.90%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
@@ -6,7 +6,6 @@ var divideArray = function(nums) {
     let result = []
     for (let num = 0; num < nums.length; num += 2) {
         let pair = nums.sort().slice(num, num + 2)
-        console.log(pair)
         if (pair[0] === pair[1]) {
             result.push(true)
         } else {
