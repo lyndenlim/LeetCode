4c3e287b90f36fff03fa9ddda31106e2ee4aed7c
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-05-23 02:47:30 -0400

    Time: 82 ms (46.64%), Space: 43.5 MB (13.92%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
@@ -4,12 +4,12 @@
  * @return {number}
  */
 var prefixCount = function(words, pref) {
-    let results = 0
+    let resultCounter = 0
     for (let word of words) {
-        if (word.startsWith(pref)) {
-            results += 1
-        } 
-    } 
-    return results
+        if (word.slice(0, pref.length) === pref) {
+            resultCounter ++
+        }
+    }
+    return resultCounter
 
 };
