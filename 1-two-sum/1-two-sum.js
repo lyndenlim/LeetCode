eae1bee3e151d36c0bb02e55e5eae41567cc027d
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-05-28 12:16:08 -0400

    Time: 135 ms (37.07%), Space: 42.6 MB (51.61%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,18 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
-   let targetMinusNum = {}
-    for (let num of nums) {
-        if (target - num in targetMinusNum) {
-            if (nums.indexOf(target - num) === nums.indexOf(num)) {
-                return [nums.indexOf(target - num), nums.indexOf(num, nums.indexOf(num) + 1)]
-            } else {
-                return [nums.indexOf(target - num), nums.indexOf(num)]
-            }
-        } else {
-            targetMinusNum[num] = true
-        }
+    let result = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in result) {
+      return [i, result[target - nums[i]]];
+    } else {
+      result[nums[i]] = i;
     }
-    return []
+  }
 };
