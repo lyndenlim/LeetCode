12df2157b259bae19008c897cb9571f19c922b39
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-05-28 16:45:13 -0400

    Time: 77 ms (74.24%), Space: 44 MB (74.10%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+
+  let mergedList = new ListNode();
+  let pointer = mergedList;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      pointer.next = list1;
+      pointer = list1;
+      list1 = list1.next;
+    } else {
+      pointer.next = list2;
+      pointer = list2;
+      list2 = list2.next;
+    }
+  }
+
+  if (!list1) pointer.next = list2;
+  if (!list2) pointer.next = list1;
+    return mergedList.next
+};
