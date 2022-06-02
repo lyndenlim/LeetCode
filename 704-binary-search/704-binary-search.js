de6b53a0254fffd72003fe035256f8ab8f4df4f0
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-02 18:51:16 -0400

    Time: 111 ms (15.88%), Space: 45.4 MB (21.01%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,24 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    // return the index where the target is found, if it's not found return -1
-    // create a index variable
-    // starting at the middle of the array, index variable should hold a value of half of the length of the array
-    // check from the middle, if the value is bigger than the target we decrement the index, otherwise we incrementing the index 
-    // return the index when the target is found, else -1 
-    
-    if (!nums.includes(target)) return -1
-    
-    let idx = Math.floor(nums.length / 2 )
-    
-   while (nums[idx] !== target ) {
-       if (nums[idx] > target) {
-           idx--
-       } else if (nums[idx] < target) {
-           idx++
-       } 
-   }
-    return idx
+    let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+      let mid = Math.floor((left+right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left =  mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
         
     
 };
