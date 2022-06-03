fdd47e4d9ea93e596294bf078719511c8ffe26b4
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-02 20:27:46 -0400

    Time: 96 ms (69.23%), Space: 49.9 MB (95.78%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,15 +3,18 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-   if (nums.length === 1) return nums[0];
+    // initialize a result variable and a currentMax variable 
+    // we set the result variable to -infinity to account for negative numbers
+    // iterate through the array and check if the number is less than 0, reset the currentMax to 0 
+    // we want the max of the current num vs the currentMax
 
-  let sum = 0;
-  let maxSum = -Infinity;
-
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
-    maxSum = Math.max(maxSum, sum);
-    if (sum < 0) sum = 0;
-  }
-  return maxSum;
+    let currentMax = 0
+    let maxSum = -Infinity
+    
+    for (i = 0; i < nums.length; i++){
+        currentMax += nums[i]
+        maxSum = Math.max(currentMax, maxSum)        
+        currentMax = currentMax < 0 ? 0 : currentMax
+    }
+    return maxSum
 };
