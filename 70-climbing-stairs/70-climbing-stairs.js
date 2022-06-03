e41dc08542107293fe3a8a766deab738c7744d2d
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-03 01:18:19 -0400

    Time: 65 ms (77.16%), Space: 41.5 MB (89.82%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let oneStep = 1;
-  let twoStep = 1;
-
-  for (let i = 0; i < n - 1; i++) {
-    let temp = oneStep;
-    oneStep = oneStep + twoStep;
-    twoStep = temp;
-  }
-  return oneStep;
+    // fibonacci seq.
+    
+    let oneStep = 1
+    let twoStep = 1
+    
+    for (let i = 0; i < n - 1; i ++) {
+        let temp = oneStep
+        oneStep = oneStep + twoStep
+        twoStep = temp
+    }
+    return oneStep
 };
