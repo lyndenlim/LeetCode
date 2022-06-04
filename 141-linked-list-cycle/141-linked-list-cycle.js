37ac6c999fa480e4ae14bdfad1cbe0f2e8f140e2
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-04 19:13:19 -0400

    Time: 79 ms (84.99%), Space: 44.7 MB (74.99%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -10,9 +10,9 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {    
-    let slow = head
-    let fast = head
+var hasCycle = function(head) {
+    let slow = head 
+    let fast = head 
     
     while (fast && fast.next) {
         slow = slow.next
