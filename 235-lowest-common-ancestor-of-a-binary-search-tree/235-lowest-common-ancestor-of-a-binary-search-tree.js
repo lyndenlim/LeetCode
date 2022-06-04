68569f2d30746fd5025f8661902a76a48b558357
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-04 19:07:59 -0400

    Time: 94 ms (76.47%), Space: 52 MB (73.93%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,19 +13,13 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    // we can check the vals of each node to see how they compare against the current node 
-    // if both p and q are greater than the current node, we just have to check the right side 
-    // if they are both less than the current node, check the left
-    // once there's a diversion/the else case we can just return the current node as the lowest common ancestor
-    
     while (root) {
-        if (root.val < p.val && root.val < q.val) {
-            root = root.right
-        } else if (root.val > p.val && root.val > q.val) {
+        if (root.val > p.val && root.val > q.val) {
             root = root.left
+        } else if (root.val < p.val && root.val < q.val) {
+            root = root.right
         } else {
             return root
         }
     }
-    
 };
