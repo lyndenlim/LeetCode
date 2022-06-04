61a653307a8e874cd33014c150205c46f03033ce
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-04 02:43:47 -0400

    Time: 74 ms (74.63%), Space: 45.1 MB (47.17%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -5,16 +5,16 @@
  */
 var search = function(nums, target) {
     let left = 0
-    let right = nums.length
+    let right = nums.length - 1
     
     while (left <= right) {
         let mid = Math.floor((left + right ) / 2)
-        if (nums[mid] === target) {
-            return mid
+        if (nums[mid] < target) {
+            left = mid + 1
         } else if (nums[mid] > target) {
             right = mid - 1 
         } else {
-            left = mid + 1 
+            return mid
         }
     }
     return -1
