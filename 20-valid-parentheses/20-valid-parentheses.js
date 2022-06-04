c3c7e3a81327287ecf2826a740caa87ca7a0e28e
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-04 02:25:57 -0400

    Time: 108 ms (18.85%), Space: 42.7 MB (23.03%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,15 +3,16 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    if (s.length % 2 !== 0) return false;
-  const parentheses = { "{": "}", "[": "]", "(": ")" };
-  let result = [];
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] in parentheses) {
-      result.push(s[i]);
-    } else if (parentheses[result.pop()] !== s[i]) {
-      return false;
+    if (s.length % 2 !==0) return false
+    
+    let parentheses = {"{": "}", "[":"]", "(":")"}
+    let bracketArray = []
+    for (bracket of s) {
+        if (bracket in parentheses) {
+            bracketArray.push(bracket)
+        } else if (bracket !== parentheses[bracketArray.pop()]) {
+            return false
+        }
     }
-  }
-  return result.length === 0;
+    return bracketArray.length === 0;
 };
