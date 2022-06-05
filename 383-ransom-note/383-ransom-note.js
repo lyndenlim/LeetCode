7920425298d715f664e84e23d928c4b0c7df8ea6
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-04 23:56:42 -0400

    Time: 123 ms (47.14%), Space: 44.7 MB (68.19%) - LeetHub

--- 383-ransom-note/383-ransom-note.js
+++ 383-ransom-note/383-ransom-note.js
@@ -3,30 +3,25 @@
  * @param {string} magazine
  * @return {boolean}
  */
-var canConstruct = function(ransomNote, magazine) {
-    // return true if ransomnote can be made from magazine string
-    // create a hashmap that will store the letter count from magazine 
-    // iterate through ransomnote and every time a letter from random note appears, we can subtract the value from magazine 
-    // if the value ever hits -1 in magazine return false
-    // otherwise true
+var canConstruct = function(ransomNote, magazine) {    
+    let magazineLetterCount = {}
     
-    let magazineLetterCount = {};
-
-  for (let i = 0; i < magazine.length; i++) {
-    if (magazineLetterCount[magazine[i]]) {
-      magazineLetterCount[magazine[i]] += 1;
-    } else {
-      magazineLetterCount[magazine[i]] = 1;
+    for (let i of magazine) {
+        if (magazineLetterCount[i] ) {
+            magazineLetterCount[i] += 1 
+        } else {
+            magazineLetterCount[i] = 1
+        }
     }
-  }
-
-  for (let letter of ransomNote) {
-    if (letter in magazineLetterCount) {
-      magazineLetterCount[letter] -= 1;
-      if (magazineLetterCount[letter] === -1) return false;
-    } else {
-      return false;
+    
+    for (let i of ransomNote) {
+        if (i in magazineLetterCount) {
+            magazineLetterCount[i] -= 1
+            if (magazineLetterCount[i] === -1) return false
+        } else {
+        return false    
+        }
+        
     }
-  }
-  return true;
+    return true
 };
