6bc0ade5acc8819e0125da9797d6260dfc223b2e
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-08 14:26:08 -0400

    Time: 110 ms (54.76%), Space: 42.8 MB (44.30%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,17 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // return the indices that add up to the target num
-    // create a hashmap that will store the difference between the current num
-    // and the target num as its key and the value being the index
-    // we can then iterate through the map and check if the difference is already   within the map, if it is, return the current element along with the value of corresponding key in the map
+    let numsInfo = {}
     
-    let numberInfo = {}
     for (let i = 0; i < nums.length; i++) {
-        if (target - nums[i] in numberInfo) {
-            return [i, numberInfo[target- nums[i]]]
+        if (target - nums[i] in numsInfo) {
+            return [i, numsInfo[target-nums[i]]]
         } else {
-            numberInfo[nums[i]] = i
+            numsInfo[nums[i]] = i
         }
     }
 };
