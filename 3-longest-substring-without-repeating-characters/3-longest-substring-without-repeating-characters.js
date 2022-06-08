5603ae10037065016b665ef105ef4dd0184a351c
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-08 14:22:23 -0400

    Time: 147 ms (47.52%), Space: 50.1 MB (15.60%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,27 +3,26 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let maximumLength = 0
-    let currentLength = 0
-    let letterCount = {}
-    let left = 0
-    
-    let i = 0
-    while (i < s.length) {
-        if (letterCount[s[i]]) {
-            while (letterCount[s[i]]) {
-                delete letterCount[s[left]]
-                left ++
-                currentLength--
-            }
-        } else {
-            letterCount[s[i]] = 1 
-            currentLength ++
-            maximumLength = Math.max(currentLength, maximumLength)
-            i ++
-        }
+     let currentLength = 0;
+  let maxLength = 0;
+  let left = 0;
+  let letterCount = {};
+
+  let i = 0;
+  while (i < s.length) {
+    if (letterCount[s[i]]) {
+      while (letterCount[s[i]]) {
+        delete letterCount[s[left]];
+        left++;
+        currentLength--;
+      }
+    } else {
+      letterCount[s[i]] = 1;
+      currentLength++;
+      maxLength = Math.max(currentLength, maxLength);
+      i++;
     }
-    
-    return maximumLength
-    
+  }
+
+  return maxLength;
 };
