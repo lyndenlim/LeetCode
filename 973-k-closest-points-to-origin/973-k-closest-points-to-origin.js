1cfd5caecae3e02a72cbb323aaf0190cd95a2a89
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-07 23:41:42 -0400

    Time: 311 ms (47.48%), Space: 67.4 MB (15.20%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -4,14 +4,15 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    let smallestDist = 0
-    let currentDist = 0 
-    let result = []
-    for (let i = 0; i < points.length; i++) {
-        currentDist = Math.sqrt(points[i][0] ** 2 + points[i][1] ** 2)
-        result.push([currentDist, points[i]])
-    }
-    
-    result.sort((a,b) => a[0] - b[0])
-    return result.slice(0,k).map(item => item[1])
+   let coordDistanceArray = [];
+
+  for (let i = 0; i < points.length; i++) {
+    coordDistanceArray.push([
+      Math.sqrt(points[i][0] ** 2 + points[i][1] ** 2),
+      points[i],
+    ]);
+  }
+
+  coordDistanceArray.sort((a, b) => a[0]- b[0]);
+  return coordDistanceArray.slice(0, k).map((pair) => pair[1]);
 };
