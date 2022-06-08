67b965f2f5be4e4437e647a495b5307f89b9d5af
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-08 18:08:41 -0400

    Time: 260 ms (68.86%), Space: 60.9 MB (42.00%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -3,11 +3,11 @@
  * @param {number} k
  * @return {number[][]}
  */
-var kClosest = function(points, k) {
-     points.sort((a, b) => squaredDistance(a) - squaredDistance(b));
-  return points.slice(0, k);
-
-  function squaredDistance([x, y]) {
-    return x ** 2 + y ** 2;
-  }
+var kClosest = function(points, k) {    
+    function getDistance([x,y]) {
+        return Math.sqrt(x ** 2 + y ** 2)
+    }
+    
+    points.sort((a,b) => getDistance(a) - getDistance(b))
+    return points.slice(0,k)
 };
