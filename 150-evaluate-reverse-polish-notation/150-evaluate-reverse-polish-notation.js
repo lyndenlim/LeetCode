5a525540b8274a944b5144b8c9fd848c060edf76
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-09 16:31:06 -0400

    Time: 104 ms (43.85%), Space: 45.7 MB (32.53%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+     let tokenStack = [];
+  for (let idx = 0; idx < tokens.length; idx++) {
+    if (tokens[idx] === "+") {
+      tokenStack.push(tokenStack.pop() + tokenStack.pop());
+    } else if (tokens[idx] === "-") {
+      let a = tokenStack.pop();
+      let b = tokenStack.pop();
+      tokenStack.push(b - a);
+    } else if (tokens[idx] === "/") {
+      let a = tokenStack.pop();
+      let b = tokenStack.pop();
+      tokenStack.push(parseInt(b / a));
+    } else if (tokens[idx] === "*") {
+      tokenStack.push(tokenStack.pop() * tokenStack.pop());
+    } else {
+      tokenStack.push(parseInt(tokens[idx]));
+    }
+  }
+  return tokenStack[0];
+};
