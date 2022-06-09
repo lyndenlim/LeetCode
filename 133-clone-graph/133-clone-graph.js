eb7684b876739dbc1e30fabe27e083f8a689f053
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-09 15:20:23 -0400

    Time: 89 ms (44.62%), Space: 44.3 MB (8.56%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.js
@@ -0,0 +1,31 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+    var visited = {};
+
+  let dfs = function (node) {
+    if (!node) return node;
+    if (visited[node.val]) return visited[node.val];
+
+    let root = new Node(node.val);
+    visited[node.val] = root;
+
+    for (let neighbor of node.neighbors) {
+      root.neighbors.push(dfs(neighbor));
+    }
+
+    return root;
+  };
+
+  return dfs(node);
+};
