ab336873b13212730ae4f33baa97d3debcc3343b
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-09 14:47:27 -0400

    Time: 148 ms (47.27%), Space: 49.9 MB (16.81%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let left = 0
     let currentLen = 0
-    let maxLen = 0 
+    let maxLen = 0
+    let left = 0
     let letterCount = {}
     
-    let i = 0
+    let i = 0 
     while (i < s.length) {
-        if (letterCount[s[i]]) {
-            while (letterCount[s[i]]){
+        if (letterCount[s[i]]) { 
+            while(letterCount[s[i]]) {
                 delete letterCount[s[left]]
                 left ++ 
-                currentLen --    
+                currentLen --
             }
         } else {
             letterCount[s[i]] = 1
             currentLen ++
-            maxLen  = Math.max(currentLen, maxLen)
+            maxLen = Math.max(currentLen, maxLen)
             i ++
         }
     }
