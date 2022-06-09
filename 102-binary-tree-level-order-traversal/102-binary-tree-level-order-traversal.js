7ca1f23777f55db23fc37ffbe53bc3af6eaca805
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-09 15:05:22 -0400

    Time: 121 ms (10.59%), Space: 44.5 MB (30.84%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -11,19 +11,26 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    if (!root) return [];
-      let q = [root],
-    result = [];
-  while (q.length > 0 ) {
-    let qlen = q.length,
-      row = [];
-    for (let i = 0; i < qlen; i++) {
-      let curr = q.shift();
-      row.push(curr.val);
-      if (curr.left) q.push(curr.left);
-      if (curr.right) q.push(curr.right);
-    }
-    result.push(row);
-  }
-  return result;
+    // bfs 
+    // create queue and we'll initialize it with the root node
+    // as long as there's something in the queue to be processed we can remove the first node and then add the children node to the queue 
+    // we can keep track of the levels by taking the length of the queue 
+    
+        if (!root) return []
+    
+        let queue = [root]
+        let result = []
+        
+        while (queue.length > 0) {
+            let qlen = queue.length
+            let rows = []
+            for (let i = 0; i < qlen; i++) {
+                let currentNode = queue.shift()
+                rows.push(currentNode.val)
+                if (currentNode.left) queue.push(currentNode.left)
+                if (currentNode.right) queue.push(currentNode.right)
+            }
+            result.push(rows)
+        }
+        return result
 };
