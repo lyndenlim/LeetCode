2f9bf4a8c7f7f627f4248ee7cafbb12cac451e96
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-10 15:28:46 -0400

    Time: 109 ms (16.79%), Space: 42.1 MB (16.90%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -18,24 +18,17 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        // create two variables, a left and a right 
-        // the left starts at 1 and the right starts at n 
-        // while the left is less than or equal to the right initialize a mid variable that will be made up of the half the sum of the left and right
-        // we can check if the mid element isBadVersion 
-        // if it is, we have to decrement the right pointer to the element right before the mid point else we increment the left pointer up to one element after the mid pointer (since we know that the versions after, before it cant be included based on our parameters)
-        // once we exit out loop we can return the left pointer 
-        
         let left = 1
         let right = n
         
         while (left <= right) {
-            let mid = Math.floor((left + right) / 2)
+            let mid = Math.floor((left + right) / 2 )
             if (isBadVersion(mid)) {
                 right = mid - 1
             } else {
-                left = mid + 1 
+                left = mid + 1
             }
         }
-        return left
+        return left 
     };
 };
