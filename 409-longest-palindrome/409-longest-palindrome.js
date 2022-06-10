3a98249e56c8f9d30fe8e62d2f82b4eb350e95a1
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-10 15:57:26 -0400

    Time: 83 ms (64.06%), Space: 44 MB (60.20%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.js
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -3,26 +3,27 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    let letterCount = {}
-    let result = 0 
+    let sLetterCount = {}
     let odd = false
+    let result = 0 
     
-    for (let i of s) {
-        if (letterCount[i]) {
-            letterCount[i] += 1
+    for (let char of s) {
+        if (sLetterCount[char]) {
+            sLetterCount[char] += 1
         } else {
-            letterCount[i] = 1
+            sLetterCount[char] = 1
         }
     }
     
-     for (let i in letterCount) {
-         if (letterCount[i] % 2 === 0) {
-             result += letterCount[i]
-         } else {
-             result += letterCount[i] - 1
-             odd = true
-         }
-     }
+    for (let i in sLetterCount) {
+        if (sLetterCount[i] % 2 === 0) {
+            result += sLetterCount[i]
+        } else {
+            result += sLetterCount[i] - 1 
+            odd = true
+        }
+    }
     
-    return odd ? result + 1: result
+    return odd ? result + 1: result 
+    
 };
