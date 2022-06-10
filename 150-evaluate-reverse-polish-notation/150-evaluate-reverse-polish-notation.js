e4b87f7eea1318bcd22cd0e48f48b41addf42479
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-09 21:31:31 -0400

    Time: 86 ms (69.32%), Space: 45.3 MB (46.56%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-     let tokenStack = [];
-  for (let idx = 0; idx < tokens.length; idx++) {
-    if (tokens[idx] === "+") {
-      tokenStack.push(tokenStack.pop() + tokenStack.pop());
-    } else if (tokens[idx] === "-") {
-      let a = tokenStack.pop();
-      let b = tokenStack.pop();
-      tokenStack.push(b - a);
-    } else if (tokens[idx] === "/") {
-      let a = tokenStack.pop();
-      let b = tokenStack.pop();
-      tokenStack.push(Math.trunc(b / a));
-    } else if (tokens[idx] === "*") {
-      tokenStack.push(tokenStack.pop() * tokenStack.pop());
-    } else {
-      tokenStack.push(parseInt(tokens[idx]));
+    let tokenStack = []
+    
+    for (let i =0; i < tokens.length; i++) {
+        if (tokens[i] === "+") {
+            tokenStack.push(tokenStack.pop() + tokenStack.pop())
+        } else if (tokens[i] === "-") {
+            let first = tokenStack.pop()
+            let second = tokenStack.pop()
+            tokenStack.push(second - first)
+        } else if (tokens[i] === "/") {
+            let first = tokenStack.pop()
+            let second = tokenStack.pop()
+            tokenStack.push(Math.trunc(second / first))
+        } else if (tokens[i] === "*") {
+            tokenStack.push(tokenStack.pop() * tokenStack.pop())
+        } else {
+            tokenStack.push(parseInt(tokens[i]))
+        }
     }
-  }
-  return tokenStack[0];
+    return tokenStack[0]
 };
