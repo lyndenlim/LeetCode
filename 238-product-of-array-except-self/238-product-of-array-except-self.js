785b9ddbbae42a5a157900e5895354c0a9a728d4
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-11 01:07:59 -0400

    Time: 100 ms (96.22%), Space: 53.9 MB (71.60%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let answer = Array(nums.length);
+  answer.fill(1);
+
+  let prefix = 1;
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let postfix = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    answer[i] *= postfix;
+    postfix *= nums[i];
+  }
+
+  return answer;
+};
