cffe7bd7b8f5ffde5809ed199aeaf6220541a670
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-11 16:21:14 -0400

    Time: 108 ms (34.13%), Space: 46.1 MB (76.97%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.js
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -11,16 +11,18 @@
  * @return {boolean}
  */
 var isValidBST = function(root) {
-    
     function dfs(node, min = null, max = null) {
-        if (!node) return true
-        
-        if ((min !== null && node.val <= min) || (max !== null && node.val >= max)) {
-            return false
-        }
-        
-        return dfs(node.left, min, node.val) && dfs(node.right, node.val, max)
+    if (!node) return true;
+
+    if (
+      (min !== null && node.val <= min) ||
+      (max !== null && node.val >= max)
+    ) {
+      return false;
     }
-    
-    return dfs(root)
+
+    return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
+  }
+
+  return dfs(root);
 };
