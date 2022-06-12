ced7207d45c4a10c9facb5bda6cc4bbe833ca004
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-12 01:51:35 -0400

    Time: 88 ms (38.75%), Space: 42.3 MB (31.43%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -4,28 +4,30 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0
-    let right = nums.length - 1
+    let left = 0 
+    let right = nums.length - 1 
     
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
-        if (target === nums[mid]) {
-            return mid
+        
+        if (nums[mid] === target) {
+            return mid 
         }
         
         if (nums[left] <= nums[mid]) {
-            if (nums[mid] < target || nums[left] > target) {
-                left = mid + 1
+            if (target < nums[left] || nums[mid] < target) {
+                left = mid + 1    
             } else {
-                right = mid - 1
-            }    
+                right = mid - 1 
+            }
         } else {
-            if (nums[mid] > target || nums[right] < target) {
+            if (target > nums[right] || nums[mid] > target) {
                 right = mid - 1 
             } else {
                 left = mid + 1 
             }
-        }   
+        }
     }
-    return -1 
+    
+    return -1
 };
