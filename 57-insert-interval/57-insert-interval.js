34e6d9f8a84babd6ae0e43d5c36dcc40785cc267
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-12 19:00:34 -0400

    Time: 85 ms (73.91%), Space: 44.4 MB (48.02%) - LeetHub

--- 57-insert-interval/57-insert-interval.js
+++ 57-insert-interval/57-insert-interval.js
@@ -6,7 +6,7 @@
 var insert = function(intervals, newInterval) {
     let result = []
     
-    for (let i = 0; i < intervals.length; i++) {
+    for (i = 0; i < intervals.length; i++) {
         if (newInterval[1] < intervals[i][0]) {
             result.push(newInterval)
             return result.concat(intervals.slice(i, intervals.length))
@@ -16,7 +16,8 @@ var insert = function(intervals, newInterval) {
             newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])]
         }
     }
-    result.push(newInterval)
     
-    return result
+    result.push(newInterval);
+    
+    return result;
 };
