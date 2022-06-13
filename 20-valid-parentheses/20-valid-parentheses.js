6c1db2b2dde0f95cad1bff5ea0f03b931c76d2cb
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-13 17:19:55 -0400

    Time: 115 ms (14.66%), Space: 41.9 MB (88.49%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -6,12 +6,12 @@ var isValid = function(s) {
     let parentheses = {"{":"}", "[":"]", "(":")"}
     let parenthesesArray = []
     
-    for (let i of s) {
-        if (i in parentheses) {
-            parenthesesArray.push(i)
-        } else if (parentheses[parenthesesArray.pop()] !== i) {
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] in parentheses) {
+            parenthesesArray.push(s[i])
+        } else if (parentheses[parenthesesArray.pop()] !== s[i]) {
             return false
-        }
+        } 
     }
     
     return parenthesesArray.length === 0 
