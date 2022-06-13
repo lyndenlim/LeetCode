930d4e18613b3065e4d1d91e4e8d52402ace1ea7
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-13 17:59:56 -0400

    Time: 77 ms (79.31%), Space: 45 MB (71.10%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -11,8 +11,15 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (!root) return 0 
-    let left = maxDepth(root.left)
-    let right = maxDepth(root.right)
-    return 1 + Math.max(left, right)
+    
+    function dfs(node) {
+        if (!node) return 0
+        
+        let left = dfs(node.left)
+        let right = dfs(node.right)
+        
+        return 1 + Math.max(left, right)
+    }
+    
+    return dfs(root)
 };
