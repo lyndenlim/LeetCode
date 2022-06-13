e0376f5e2af0e8f96d0ed70025100f41f9fb013b
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-13 18:32:02 -0400

    Time: 89 ms (84.97%), Space: 50.9 MB (19.13%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -6,10 +6,10 @@ var maxSubArray = function(nums) {
     let currentMax = 0
     let totalMax = nums[0] 
     
-    for (let i = 0; i < nums.length; i++) {
-        currentMax += nums[i]
-        totalMax = Math.max(totalMax, currentMax)
-        currentMax = currentMax < 0 ? 0 : currentMax 
+    for (let num of nums) {
+        currentMax += num
+        totalMax = Math.max(currentMax, totalMax)
+        currentMax = currentMax < 0 ? 0 : currentMax
     }
     
     return totalMax
