e6263f85293a5dc33cfa4b98e809079fc5cb3b74
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-14 09:15:18 -0400

    Time: 225 ms (13.27%), Space: 49.2 MB (30.76%) - LeetHub

--- 207-course-schedule/207-course-schedule.js
+++ 207-course-schedule/207-course-schedule.js
@@ -8,7 +8,7 @@ var canFinish = function(numCourses, prerequisites) {
     let queue = []
     
     for (let [course, prereq] of prerequisites) {
-        indegrees[course] += 1 
+        indegrees[course] += 1
     }
     
     for (let i = 0; i < indegrees.length; i++) {
@@ -17,21 +17,21 @@ var canFinish = function(numCourses, prerequisites) {
         }
     }
     
-    let count = 0 
+    let count = 0
     
     while (queue.length > 0) {
         let current = queue.shift()
-        count += 1 
+        count += 1
         
         for (let [course, prereq] of prerequisites) {
-            if (current === prereq) {
-                indegrees[course] -= 1
+            if (prereq === current) {
+                indegrees[course] -= 1 
                 if (indegrees[course] === 0) {
-                    queue.push(course)    
+                    queue.push(course)
                 }
             }
         }
     }
     
-    return numCourses === count
+    return count === numCourses
 };
