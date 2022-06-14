3b844694027f8fa08b9d1326b98576a85121c98a
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-14 12:32:51 -0400

    Time: 117 ms (7.66%), Space: 42.5 MB (16.41%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -3,30 +3,28 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-        let left = 0
-        let right = nums.length - 1 
-        
-        while (left <= right) {
-            let mid = Math.floor((left + right) / 2)
-            if (nums[mid] === target) {
-                return mid
-            }
-            
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] > target || nums[mid] < target) {
-                    left = mid + 1 
-                } else {
-                    right = mid - 1 
-                }
-            } else {
-                if (nums[right] < target || nums[mid] > target) {
-                    right = mid - 1 
-                } else {
-                    left = mid + 1 
-                }
-            }
-        }
-        return -1 
+var search = function(array, target) {
+    let start = 0
+  let end = array.length - 1
+
+  while(start <= end){
+    let mid = Math.floor((start + end)/2)
+    if (array[mid] === target) return mid
+    if (array[start] <= array[mid]){ //left side is sorted
+      if (array[start] <= target && target <= array[mid]){
+        end = mid - 1
+      } else {
+        start = mid + 1
+      }
+    } else { //right side is sorted
+      if (array[mid] <= target && target <= array[end]){
+        start = mid + 1
+      } else {
+        end = mid - 1
+      }
+    }
+
     
+  }
+    return -1
 };
