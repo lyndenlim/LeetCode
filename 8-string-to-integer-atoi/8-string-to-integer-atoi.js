bf8e792b454d0beefe99f9f1768cb78214e3e18e
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-16 19:18:32 -0400

    Time: 114 ms (45.30%), Space: 45.3 MB (30.78%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.js
@@ -3,26 +3,26 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-  let i = 0;
-  let sign = 1;
-  let res = 0;
-  while (s[i] == " ") i++;
-
-  if (s[i] === "+") {
-    i++;
-  } else if (s[i] === "-") {
-    sign = -1;
-    i++;
-  }
-
-  while (s[i] >= "0" && s[i] <= "9") {
-    res = res * 10 + (s[i] - 0);
-    if (res * sign >= 2147483647) {
-      return 2147483647;
-    } else if (res * sign <= -2147483648) {
-      return -2147483648;
+    let idx = 0
+    let result = 0
+    let sign = 1
+    
+    while (s[idx] === " ") idx++ 
+        
+    if (s[idx] === "+") {
+        idx++;
+    } else if (s[idx] === "-"){
+        sign = -1 
+        idx++;
     }
-    i++;
-  }
-  return res * sign;
+    
+    while  (s[idx] >= "0" && s[idx] <= "9"){
+        result = result * 10 + (s[idx] - 0) 
+        if (result * sign >= (2 ** 31) - 1 ) return (2 ** 31) - 1
+    
+        if (result * sign <= -2147483648) return -2147483648
+        idx++;
+    }
+    
+    return result * sign
 };
