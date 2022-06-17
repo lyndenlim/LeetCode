2d336be13c697627f36441e5f0e167c27d7f8fcf
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-17 16:25:47 -0400

    Time: 82 ms (61.45%), Space: 44.2 MB (17.53%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
@@ -11,24 +11,27 @@
  * @return {number[]}
  */
 var rightSideView = function(root) {
-    let result = []
-    let queue = [root]
+    let result = [] 
+    let queue = [root] 
     
     while (queue.length) {
-        let rightSide = null
         let qlen = queue.length
+        let rightSide = null
         
-        for (let i = 0; i < qlen; i ++) {
+        for (let i = 0; i < qlen; i++) {
             let node = queue.shift()
             if (node) {
                 rightSide = node
-               queue.push(node.left)
+                queue.push(node.left)
                 queue.push(node.right)
-            }
+            }    
         }
         if (rightSide) {
             result.push(rightSide.val)
         }
     }
+    
     return result
+    
+    
 };
