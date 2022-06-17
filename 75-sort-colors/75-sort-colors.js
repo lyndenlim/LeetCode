350e2727b70cf041d4ec000db5830eb8e27c61a8
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-17 18:33:02 -0400

    Time: 67 ms (85.41%), Space: 42 MB (73.07%) - LeetHub

--- 75-sort-colors/75-sort-colors.js
+++ 75-sort-colors/75-sort-colors.js
@@ -3,26 +3,23 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let left = 0
-    let right = nums.length -1 
+    let left = 0 
+    let right = nums.length - 1 
     
-    let current = 0 
-    
-    function swap(i,j) {
-        let temp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = temp
-    }
-    
-    while (current <= right) {
-        if (nums[current] === 0) {
-            swap(left,current)
-            left += 1 
-        } else if (nums[current] === 2) {
-            swap(current,right)
-            right -= 1 
-            current -= 1 
+    let i = 0 
+    while (i <= right) {
+        if (nums[i] === 2) {
+            let temp = nums[i] 
+            nums[i] = nums[right]
+            nums[right] = temp
+            right -= 1;
+            i--;
+        } else if (nums[i] === 0) {
+            let temp = nums[left]
+            nums[left] = nums[i]
+            nums[i] = temp 
+            left += 1 ;
         }
-        current += 1 
+        i++;
     }
 };
