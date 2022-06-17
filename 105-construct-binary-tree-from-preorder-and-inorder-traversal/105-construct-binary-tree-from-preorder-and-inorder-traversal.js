924e3250a09f86c5674ba2d55c497edd208ecbc4
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-17 18:18:29 -0400

    Time: 180 ms (39.78%), Space: 119 MB (39.66%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -12,14 +12,16 @@
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
-    if (!preorder.length || !inorder.length) return null
-    
-    
-    let root = new TreeNode(preorder[0])
-    let mid = inorder.indexOf(preorder[0])
-    
-    root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid))
-    root.right = buildTree(preorder.slice(mid + 1, preorder.length), inorder.slice(mid + 1, inorder.length))
-    
-    return root
+    if (!preorder.length || !inorder.length) return null;
+
+  let root = new TreeNode(preorder[0]);
+  let mid = inorder.indexOf(preorder[0]);
+
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid + 1));
+  root.right = buildTree(
+    preorder.slice(mid + 1, preorder.length),
+    inorder.slice(mid + 1, inorder.length)
+  );
+
+  return root;
 };
