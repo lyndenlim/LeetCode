129ce0193bcdcd18de59447f25a69b158f5dc98c
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-19 17:35:32 -0400

    Time: 115 ms (46.00%), Space: 45.3 MB (30.67%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.js
@@ -3,25 +3,24 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    let idx = 0
     let result = 0
-    let sign = 1
+    let idx = 0 
+    let sign = 1 
     
-    while (s[idx] === " ") idx++ 
-        
+    while (s[idx] === " ") idx++;
+    
     if (s[idx] === "+") {
         idx++;
-    } else if (s[idx] === "-"){
+    } else if (s[idx] === "-") {
         sign = -1 
         idx++;
     }
     
-    while  (s[idx] >= "0" && s[idx] <= "9"){
-        result = result * 10 + (s[idx] - 0) 
-        if (result * sign >= (2 ** 31) - 1 ) return (2 ** 31) - 1
-    
-        if (result * sign <= Math.pow(-2, 31)) return Math.pow(-2, 31)
-        idx++;
+    while (s[idx] >= "0" && s[idx] <= "9") {
+        result = result * 10 + (s[idx] - 0)
+        if (result * sign >= Math.pow(2, 31) - 1) return Math.pow(2,31) -1
+        if (result * sign <= Math.pow(-2, 31)) return Math.pow(-2,31)
+        idx++
     }
     
     return result * sign
