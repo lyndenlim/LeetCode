07a305bd1e7d8c845f4cc73c8dc06bf9fe6d3743
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-19 11:48:22 -0400

    Time: 145 ms (17.66%), Space: 52.2 MB (61.85%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -14,13 +14,14 @@
  */
 var lowestCommonAncestor = function(root, p, q) {
     while (root) {
-        if (root.val > p.val && root.val > q.val) {
-            root = root.left
-        } else if (root.val < p.val && root.val < q.val) {
-            
+        if (root.val > q.val && root.val > p.val) {
+        root = root.left
+        } else if (root.val < q.val && root.val < p.val) {
             root = root.right
         } else {
             return root
-        }
+        }    
     }
+    
+    
 };
