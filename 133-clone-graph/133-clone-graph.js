e72021f5a07f619194f5fb2be1c44882c94ae0ec
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-19 16:38:16 -0400

    Time: 66 ms (92.80%), Space: 43.6 MB (87.09%) - LeetHub

--- 133-clone-graph/133-clone-graph.js
+++ 133-clone-graph/133-clone-graph.js
@@ -14,19 +14,19 @@ var cloneGraph = function(node) {
     let visited = {}
     
     function clone(node) {
-        if (!node) return node
+        if (!node) return node 
         
         if (visited[node.val]) return visited[node.val]
         
         let head = new Node(node.val)
-        visited[node.val] = head
+        visited[node.val] = head 
         
         for (let nei of node.neighbors) {
             head.neighbors.push(clone(nei))
         }
-        
         return head
     }
     
     return clone(node)
+    
 };
