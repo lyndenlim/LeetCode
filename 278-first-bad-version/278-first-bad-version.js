f35615940bbf044020d12363cef565ebd2d35158
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-19 14:03:33 -0400

    Time: 95 ms (31.33%), Space: 41.9 MB (41.55%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -19,16 +19,16 @@ var solution = function(isBadVersion) {
      */
     return function(n) {
         let left = 1
-        let right = n
+        let right = n 
         
         while (left <= right) {
-            let mid = Math.floor((left + right) / 2 )
+            let mid = Math.floor((left + right) / 2)
             if (isBadVersion(mid)) {
-                right = mid - 1
+                right = mid - 1 
             } else {
-                left = mid + 1
+                left = mid + 1 
             }
         }
-        return left 
+        return left
     };
 };
