33ed42abce7a109811af288f2b8329f0289cbca3
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-19 17:43:31 -0400

    Time: 204 ms (49.74%), Space: 45.9 MB (58.16%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -3,36 +3,34 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
-    let result = ""
     let maxLen = 0
+    let result = ""
     
     for (let i = 0; i < s.length; i++) {
         let left = i
-        let right = i
+        let right = i 
         
         while (left >= 0 && right <= s.length && s[left] === s[right]) {
             if (right - left + 1 > maxLen) {
-                result = s.slice(left, right + 1)
+                result = s.slice(left,right + 1)
                 maxLen = right - left + 1     
             }
-            left -= 1 
-            right += 1 
+            left--;
+            right++;
         }
         
         left = i
-        right = i + 1 
+        right = i + 1
         
         while (left >= 0 && right <= s.length && s[left] === s[right]) {
             if (right - left + 1 > maxLen) {
-                result = s.slice(left, right + 1)
+                result = s.slice(left,right + 1)
                 maxLen = right - left + 1     
             }
-            left -= 1 
-            right += 1
+            left--;
+            right++;
         }
     }
     
-    return result 
-    
+    return result
 };
