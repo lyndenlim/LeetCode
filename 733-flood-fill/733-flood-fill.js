721abf081e075867f3112d634be1f9ba0a5e8984
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-20 19:22:43 -0400

    Time: 172 ms (5.31%), Space: 44.6 MB (33.66%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -8,19 +8,18 @@
 var floodFill = function(image, sr, sc, color) {
     let currentColor = image[sr][sc]
     
-    if (currentColor === color) return image
+    if (currentColor === color) return image 
     
     let queue = [[sr,sc]]
     
     while (queue.length) {
-        let [row, column] = queue.shift()
-        
-        if (image[row][column] === currentColor) {
-            image[row][column] = color
-            if (row - 1 >= 0 ) queue.push([row -1, column])
-            if (row + 1 < image.length ) queue.push([row +1, column])
-            if (column - 1 >= 0 ) queue.push([row, column - 1])
-            if (column + 1 < image[row].length ) queue.push([row, column + 1])
+        let [row, col] = queue.shift()
+        if (image[row][col] === currentColor) {
+            image[row][col] = color
+            if (row - 1 >= 0) queue.push([row - 1, col])
+            if (row + 1 < image.length) queue.push([row + 1, col])
+            if (col - 1 >= 0) queue.push([row, col - 1])
+            if (col + 1 < image[row].length) queue.push([row, col + 1])
         }
     }
     return image
