7e57a619fbcf92a8a03185881995bb2e0ff4b44b
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-24 18:07:16 -0400

    Time: 113 ms (91.84%), Space: 46.2 MB (64.31%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -4,10 +4,10 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    let cache = Array(amount + 1).fill(amount + 1)
+    let cache = new Array(amount + 1).fill(amount + 1)
     cache[0] = 0
     
-    for (let i = 1; i <= amount; i++) {
+    for (let i = 0; i <= amount; i++) {
         for (let j = 0; j < coins.length; j++) {
             if (coins[j] <= i) {
                 cache[i] = Math.min(cache[i], cache[i - coins[j]] + 1)
