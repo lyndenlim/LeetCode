f6c33c80b8e8a28d1296b9ea410438887bb965a3
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-24 18:10:37 -0400

    Time: 141 ms (59.54%), Space: 55.6 MB (30.28%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -5,13 +5,13 @@
 var productExceptSelf = function(nums) {
     let result = []
     
-    let prefix = 1   
+    let prefix = 1
     for (let i = 0; i < nums.length; i++) {
         result[i] = prefix
         prefix *= nums[i]
     }
     
-    let suffix = 1 
+    let suffix = 1
     for (let i = nums.length - 1; i >= 0; i--) {
         result[i] *= suffix
         suffix *= nums[i]
