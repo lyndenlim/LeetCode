83c57335cc6ad6a5e3d586c9ab9ecfc5f4ece82d
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-25 19:37:37 -0400

    Time: 183 ms, Memory: 49.3 MB - LeetHub

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let squaredNums = []
+    
+    for (let i = 0; i < nums.length; i++) {
+        squaredNums.push(nums[i] ** 2)
+    }
+    
+    return squaredNums.sort((a,b) => a - b)
+};
