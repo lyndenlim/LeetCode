df62c2fad632651f06f8eff1b7768e35781cc7a0
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-24 22:28:52 -0400

    Time: 55 ms (99.39%), Space: 41.9 MB (83.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -4,10 +4,10 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let row = Array(n).fill(1)
+    let row = new Array(n).fill(1)
     
-    for (let i = 1; i < m; i++) {
-        let newRow = Array(n).fill(1)
+    for (let i = 1; i < m; i++){
+        let newRow = new Array(n).fill(1)
         for (let j = n - 2; j >= 0; j--) {
             newRow[j] = newRow[j + 1] + row[j]
         }
@@ -15,4 +15,5 @@ var uniquePaths = function(m, n) {
     }
     
     return row[0]
+    
 };
