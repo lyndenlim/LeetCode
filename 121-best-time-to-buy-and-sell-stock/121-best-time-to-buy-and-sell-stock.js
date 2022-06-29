cc1177635e37426055ff3cd70c0d113ccfedc48e
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-29 00:01:54 -0400

    Time: 87 ms (93.71%), Space: 51.7 MB (53.33%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let currentMin = prices[0]
     let maxProfit = 0
-    
-    for (let price of prices) {
-        currentMin = Math.min(currentMin, price)
-        maxProfit = Math.max(maxProfit, price - currentMin)
+    let currentMin = prices[0]
+    for (let i = 0; i < prices.length; i++) {
+        currentMin = Math.min(currentMin, prices[i])
+        maxProfit = Math.max(maxProfit, prices[i] - currentMin)
     }
     
     return maxProfit
