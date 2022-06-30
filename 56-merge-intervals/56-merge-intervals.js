86e5c7a8204e1c2650d79e27c2fe3904fd3318f8
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-06-30 17:18:42 -0400

    Time: 205 ms (16.41%), Space: 49.1 MB (61.86%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -3,12 +3,12 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    intervals.sort((a,b) => a[0]-b[0])
+    intervals.sort((a,b) => a[0] - b[0])
     
     let result = []
     result[0] = intervals[0]
     
-    for (let i = 1; i < intervals.length; i++) {
+    for (let i = 0; i < intervals.length; i++) {
         if (result[result.length-1][1] >= intervals[i][0]) {
             result[result.length-1][1] = Math.max(result[result.length-1][1], intervals[i][1])
         } else {
@@ -17,4 +17,5 @@ var merge = function(intervals) {
     }
     
     return result
+    
 };
