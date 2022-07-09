65bd45923d1eb4f298a9ca5c480644e3c8b78e00
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-07-09 02:03:26 -0400

    Time: 82 ms (93.25%), Space: 51.8 MB (50.27%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -12,13 +12,13 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if (!root || root == q || root == p) return root
+    if (!root || root == p || root == q) return root
     
     let left = lowestCommonAncestor(root.left, p, q)
     let right = lowestCommonAncestor(root.right, p, q)
     
-    if (!left) return right
     if (!right) return left
+    if (!left) return right
     
     return root
 };
