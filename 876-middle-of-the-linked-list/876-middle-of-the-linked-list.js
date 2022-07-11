7daac4f1654320541eec3d260b41f07fb0f0ff81
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-07-10 23:50:28 -0400

    Time: 98 ms (28.39%), Space: 42.2 MB (28.50%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -10,17 +10,14 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    // we can create 2 variables, a slow and a fast pointer 
-    // they both start at the head and move at different speeds throughout the list 
-    // since the fast pointer will always reach the end of the list before the slow we can check for its presence in a loop and while it's still present we increment each pointer by 1 and 2 nodes respectively 
-    // when the fast node no longer exists, we exit the loop and we can return the slow pointer since this end up being the middle of the list
-    
-    let slow = head 
-    let fast = head
+    let slow = head
+    let fast = head 
     
     while (fast && fast.next) {
         slow = slow.next
         fast = fast.next.next
     }
+    
     return slow
+    
 };
