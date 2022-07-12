ca4863d3fb1751b4c575ae0a343f385d36443072
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-07-11 22:57:42 -0400

    Time: 133 ms (71.63%), Space: 56.3 MB (17.19%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,20 +3,21 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let result = []
+    let answer = []
     
     let prefix = 1
     for (let i = 0; i < nums.length; i++) {
-        result[i] = prefix
+        answer[i] = prefix 
         prefix *= nums[i]
-    }
-    
-    let suffix = 1
-    for (let i = nums.length - 1; i >= 0; i--) {
-        result[i] *= suffix
-        suffix *= nums[i]
         
     }
     
-    return result
+    let postfix = 1
+    for (let i = nums.length - 1; i >= 0; i--) {
+        answer[i] *= postfix
+        postfix *= nums[i]
+    }
+    
+    return answer
+    
 };
