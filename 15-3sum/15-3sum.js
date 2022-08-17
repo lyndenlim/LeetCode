967f52b58115b01c5a8183e36b6112e4e0f84665
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2022-08-17 17:20:41 -0400

    Time: 214 ms (60.88%), Space: 52.6 MB (62.36%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,32 +3,34 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a,b) => a-b)
+    nums.sort((a,b) => a - b)
     
     let result = []
     
-    for (let i = 0; i< nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i-1]) {
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] === nums[i - 1] && i > 0) {
             continue
         }
         
-        let left = i + 1 
+        let left = i + 1
         let right = nums.length - 1 
         while (left < right) {
-            let threeSum = nums[i] + nums[left] + nums[right]
-            if (threeSum > 0) {
-                right--;
+            let threeSum = nums[left] + nums[right] + nums[i]
+            if (threeSum > 0) { 
+                right--
             } else if (threeSum < 0) {
-                left++;
+                left++
             } else {
-                result.push([nums[i], nums[left], nums[right]])
-                left++;
-                 while (nums[left] === nums[left - 1] && left < right) {
-                    left++;
+                result.push([nums[left], nums[right], nums[i]])
+                left++
+                while (nums[left] === nums[left - 1] && left < right) {
+                    left++
                 }
             }
         }
-    }
     
-    return result 
+                
+        
+    }
+    return result
 };
